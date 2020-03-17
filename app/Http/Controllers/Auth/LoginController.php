0ba954df8d9408df7c310c11b2c2e78cb1a6a062
Emmanuelamanga <emmanuelamanga2013@gmail.com>
2020-03-17 15:41:31 +0300

    all set at the branch

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+// use Auth;
 
 class LoginController extends Controller
 {
