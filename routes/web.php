d9823858ebcd13be32f3d70c4ec007e5f74b52c7
Emmanuelamanga <emmanuelamanga2013@gmail.com>
2020-03-20 09:51:35 +0300

    route middleware updated

--- routes/web.php
+++ routes/web.php
@@ -21,39 +21,42 @@ Route::get('/home', 'HomeController@index')->name('home');
 // categories routes
 // Route::resource('subcategory', 'PackageCategoryController');
 
-Route::get('/category', 'PackageCategoryController@index')->name('category.index');
-Route::get('/category/{id}/edit','PackageCategoryController@edit')->name('category.edit');
-Route::get('/category/{id}','PackageCategoryController@show')->name('category.show');
-Route::get('/category/{id}/delete','PackageCategoryController@destroy')->name('category.destroy');
-Route::get('/createcategory','PackageCategoryController@create')->name('category.create');
-Route::post('/storecategory','PackageCategoryController@store')->name('category.store');
-Route::post('/category/update/{id}','PackageCategoryController@update')->name('category.update');
+Route::middleware(['is_admin'])->group(function () {
+	Route::get('/category', 'PackageCategoryController@index')->name('category.index');
+	Route::get('/category/{id}/edit','PackageCategoryController@edit')->name('category.edit');
+	Route::get('/category/{id}','PackageCategoryController@show')->name('category.show');
+	Route::get('/category/{id}/delete','PackageCategoryController@destroy')->name('category.destroy');
+	Route::get('/createcategory','PackageCategoryController@create')->name('category.create');
+	Route::post('/storecategory','PackageCategoryController@store')->name('category.store');
+	Route::post('/category/update/{id}','PackageCategoryController@update')->name('category.update');
 
-// subcategory routes
-// Route::resource('subcategory', 'PackageSubcategoryController');
+	// subcategory routes
+	// Route::resource('subcategory', 'PackageSubcategoryController');
 
 
-Route::get('/subcategory', 'PackageSubcategoryController@index')->name('subcategory.index');
-Route::get('/subcategory/{id}/edit','PackageSubcategoryController@edit')->name('subcategory.edit');
-Route::get('/subcategory/{id}','PackageSubcategoryController@show')->name('subcategory.show');
-Route::get('/subcategory/{id}/delete','PackageSubcategoryController@destroy')->name('subcategory.destroy');
-Route::get('/createsubcategory','PackageSubcategoryController@create')->name('subcategory.create');
-Route::post('/storesubcategory','PackageSubcategoryController@store')->name('subcategory.store');
-Route::post('/subcategory/update/{id}','PackageSubcategoryController@update')->name('subcategory.update');
+	Route::get('/subcategory', 'PackageSubcategoryController@index')->name('subcategory.index');
+	Route::get('/subcategory/{id}/edit','PackageSubcategoryController@edit')->name('subcategory.edit');
+	Route::get('/subcategory/{id}','PackageSubcategoryController@show')->name('subcategory.show');
+	Route::get('/subcategory/{id}/delete','PackageSubcategoryController@destroy')->name('subcategory.destroy');
+	Route::get('/createsubcategory','PackageSubcategoryController@create')->name('subcategory.create');
+	Route::post('/storesubcategory','PackageSubcategoryController@store')->name('subcategory.store');
+	Route::post('/subcategory/update/{id}','PackageSubcategoryController@update')->name('subcategory.update');
 
-// subcategoryitems routes
-Route::resource('subcatitem', 'SubCatItemController');
+	// subcategoryitems routes
+	Route::resource('subcatitem', 'SubCatItemController');
 
-// subcategoryitems routes
-Route::get('search', 'searchController@search')->name('search');
+	// subcategoryitems routes
+	Route::get('search', 'searchController@search')->name('search');
 
-// materialgroup(classrooms) routes
-// Route::resource('materialgroup', 'MaterialGroupController');
+	// materialgroup(classrooms) routes
+	// Route::resource('materialgroup', 'MaterialGroupController');
 
-Route::get('/materialgroup', 'MaterialGroupController@index')->name('materialgroup.index');
-Route::get('/materialgroup/{id}/edit','MaterialGroupController@edit')->name('materialgroup.edit');
-Route::get('/materialgroup/{id}','MaterialGroupController@show')->name('materialgroup.show');
-Route::get('/materialgroup/{id}/delete','MaterialGroupController@destroy')->name('materialgroup.destroy');
-Route::get('/creatematerialgroup','MaterialGroupController@create')->name('materialgroup.create');
-Route::post('/storematerialgroup','MaterialGroupController@store')->name('materialgroup.store');
-Route::post('/materialgroup/update/{id}','MaterialGroupController@update')->name('materialgroup.update');
+	Route::get('/materialgroup', 'MaterialGroupController@index')->name('materialgroup.index');
+	Route::get('/materialgroup/{id}/edit','MaterialGroupController@edit')->name('materialgroup.edit');
+	Route::get('/materialgroup/{id}','MaterialGroupController@show')->name('materialgroup.show');
+	Route::get('/materialgroup/{id}/delete','MaterialGroupController@destroy')->name('materialgroup.destroy');
+	Route::get('/creatematerialgroup','MaterialGroupController@create')->name('materialgroup.create');
+	Route::post('/storematerialgroup','MaterialGroupController@store')->name('materialgroup.store');
+	Route::post('/materialgroup/update/{id}','MaterialGroupController@update')->name('materialgroup.update');
+});
+
