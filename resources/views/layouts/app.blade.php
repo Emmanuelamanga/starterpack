abbed96028155877b202d18ae11a56979028b603
theOracleWizard <32468563+Emmanuelamanga@users.noreply.github.com>
2020-04-08 00:27:56 +0300

    added jquery script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
  
     <!-- data tables -->
     <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.css" />
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.js"></script>
 
